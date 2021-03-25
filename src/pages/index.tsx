41300978774e597610237440a86293ae698ac7b5
pedroksty <pedrotrab2132@gmail.com>
2021-03-25 02:42:12 -0300

    Create Global styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | Ig.news</title>
+      </Head>
+
+     <h1> Hello Woorld</h1>
+
+    </>
+  )
+}
